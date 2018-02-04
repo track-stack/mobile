dc5aeb231dbbd8869d796ac768e2c35c173e6d41
Mike Kavouras <kavourasm@gmail.com>
2018-02-04 13:40:12 -0600

    auth and fb login prompt

--- App.js
+++ App.js
@@ -3,42 +3,49 @@ import { StyleSheet, Text, View } from 'react-native'
 import { Provider, connect } from 'react-redux'
 import { store } from 'trackstack'
 import TestViewContainer from './lib/components/Admin/TestViewContainer'
-
-class AppView extends React.Component {
-  render() {
-    return <TestViewContainer />
-  }
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
-
-
-const mapStateToProps = state => {
-  return {}
-}
-
-const mapDispatchToProps = dispatch => {
-  return {}
-}
-
-const ConnectedComponent = connect(
-  mapStateToProps,
-  mapDispatchToProps
-)(AppView)
+import LoginView from './lib/components/Auth/LoginView'
 
 export default class App extends React.Component {
   render() {
-    return (
-      <Provider store={store}>
-        <ConnectedComponent />
-      </Provider>
-    )
+    return <LoginView />
   }
 }
+
+// class AppView extends React.Component {
+//   render() { 
+//     return <TestViewContainer />
+//   }
+// }
+// 
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: '#fff',
+//     alignItems: 'center',
+//     justifyContent: 'center',
+//   },
+// });
+// 
+// 
+// const mapStateToProps = state => {
+//   return {}
+// }
+// 
+// const mapDispatchToProps = dispatch => {
+//   return {}
+// }
+// 
+// const ConnectedComponent = connect(
+//   mapStateToProps,
+//   mapDispatchToProps
+// )(AppView)
+
+// export default class App extends React.Component {
+//   render() {
+//     return (
+//       <Provider store={store}>
+//         <ConnectedComponent />
+//       </Provider>
+//     )
+//   }
+// }

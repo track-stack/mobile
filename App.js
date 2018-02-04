1ba98aefa745d25b1fb751cafc89b2ded7f7c1f1
Mike Kavouras <kavourasm@gmail.com>
2018-02-04 12:13:41 -0600

    admin view

---
+++ App.js
@@ -0,0 +1,44 @@
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+import { Provider, connect } from 'react-redux'
+import { store } from 'trackstack'
+import TestViewContainer from './lib/components/Admin/TestViewContainer'
+
+class AppView extends React.Component {
+  render() {
+    return <TestViewContainer />
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
+
+
+const mapStateToProps = state => {
+  return {}
+}
+
+const mapDispatchToProps = dispatch => {
+  return {}
+}
+
+const ConnectedComponent = connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(AppView)
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <Provider store={store}>
+        <ConnectedComponent />
+      </Provider>
+    )
+  }
+}

b4b41b644fb16d26ce77f0c2b60e08dff8da4bd3
Mike Kavouras <kavourasm@gmail.com>
2018-03-25 12:03:46 -0500

    most basic successful auth implementation

--- App.js
+++ App.js
@@ -2,50 +2,33 @@ import React from 'react'
 import { StyleSheet, Text, View } from 'react-native'
 import { Provider, connect } from 'react-redux'
 import { store } from 'trackstack'
-import TestViewContainer from './lib/components/Admin/TestViewContainer'
-import LoginView from './lib/components/Auth/LoginView'
+import LoginViewContainer from './lib/components/Auth/LoginViewContainer'
 
-export default class App extends React.Component {
+class AppView  extends React.Component {
   render() {
-    return <LoginView />
+    return <LoginViewContainer />
   }
 }
 
-// class AppView extends React.Component {
-//   render() { 
-//     return <TestViewContainer />
-//   }
-// }
-// 
-// const styles = StyleSheet.create({
-//   container: {
-//     flex: 1,
-//     backgroundColor: '#fff',
-//     alignItems: 'center',
-//     justifyContent: 'center',
-//   },
-// });
-// 
-// 
-// const mapStateToProps = state => {
-//   return {}
-// }
-// 
-// const mapDispatchToProps = dispatch => {
-//   return {}
-// }
-// 
-// const ConnectedComponent = connect(
-//   mapStateToProps,
-//   mapDispatchToProps
-// )(AppView)
+const mapStateToProps = state => {
+  return {}
+}
 
-// export default class App extends React.Component {
-//   render() {
-//     return (
-//       <Provider store={store}>
-//         <ConnectedComponent />
-//       </Provider>
-//     )
-//   }
-// }
+const mapDispatchToProps = dispatch => {
+  return {}
+}
+
+const ConnectedComponent = connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(AppView)
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <Provider store={store}>
+        <ConnectedComponent />
+      </Provider>
+    )
+  }
+}
